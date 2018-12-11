8ac0db20abd211ddb4f60dbd541e1a650f6b9d5a
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-12-10 22:51:56 -0800

    chore: update project name

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -8,7 +8,7 @@ export default class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={images.logo} className="App-logo" alt="logo" />
-          <h5 style={{ fontSize: '2em' }}>React Boilerplate</h5>
+          <h5 style={{ fontSize: '2em' }}>Wataru Maeda | Portfolio</h5>
           <p>
             Edit <code>src/Home.js</code> and save to reload.
           </p>

ef52bfd4a4680f5cad3bb7c29fe84d80fc537426
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2019-01-07 09:46:45 -0800

    chore: update fonts

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,10 +4,10 @@ import greattaractive from './images/icon-greattaractive.png'
 import iosClassForBeginners from './images/ios-class-for-beginners.png'
 import comingSoon from './images/coming-soon.svg'
 
-import regular from './fonts/Arvo-Regular.ttf'
-import italic from './fonts/Arvo-Italic.ttf'
-import bold from './fonts/Arvo-Regular.ttf'
-import boldItalic from './fonts/Arvo-BoldItalic.ttf'
+import regular from './fonts/SF-Regular.otf'
+import italic from './fonts/SF-Italic.otf'
+import bold from './fonts/SF-Regular.otf'
+import boldItalic from './fonts/SF-BoldItalic.otf'
 
 // MARK: - Images
 

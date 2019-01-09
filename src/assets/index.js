4921d913fc58b6d9cded9dcb6e177b1c6d7b77f3
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2019-01-09 10:14:45 -0800

    chore: change publishments design

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,39 @@
-import flashcard from './images/icon-share-flash-cards.png'
-import stickyMemo from './images/icon-sticky-memo.png'
-import greattaractive from './images/icon-greattaractive.png'
-import iosClassForBeginners from './images/ios-class-for-beginners.png'
-import comingSoon from './images/coming-soon.svg'
+import flashcardIcon from './images/icon-share-flash-cards.png'
+import stickyMemoIcon from './images/icon-sticky-memo.png'
+import greattaractiveIcon from './images/icon-greattaractive.png'
+import iosClassForBeginnersIcon from './images/ios-class-for-beginners.png'
+import comingSoonIcon from './images/coming-soon.svg'
+
+import ss_en_1_flashCard from './images/ss-flash-cards/ss_1_en.png'
+import ss_en_2_flashCard from './images/ss-flash-cards/ss_2_en.png'
+import ss_en_3_flashCard from './images/ss-flash-cards/ss_3_en.png'
+import ss_en_4_flashCard from './images/ss-flash-cards/ss_4_en.png'
+import ss_en_5_flashCard from './images/ss-flash-cards/ss_5_en.png'
+import ss_jp_1_flashCard from './images/ss-flash-cards/ss_1_jp.png'
+import ss_jp_2_flashCard from './images/ss-flash-cards/ss_2_jp.png'
+import ss_jp_3_flashCard from './images/ss-flash-cards/ss_3_jp.png'
+import ss_jp_4_flashCard from './images/ss-flash-cards/ss_4_jp.png'
+import ss_jp_5_flashCard from './images/ss-flash-cards/ss_5_jp.png'
+
+import ss_en_1_stickyMemo from './images/ss-sticky-memo/ss_1_en.png'
+import ss_en_2_stickyMemo from './images/ss-sticky-memo/ss_2_en.png'
+import ss_en_3_stickyMemo from './images/ss-sticky-memo/ss_3_en.png'
+import ss_jp_1_stickyMemo from './images/ss-sticky-memo/ss_1_jp.png'
+import ss_jp_2_stickyMemo from './images/ss-sticky-memo/ss_2_jp.png'
+import ss_jp_3_stickyMemo from './images/ss-sticky-memo/ss_3_jp.png'
+
+import ss_en_1_greattaractive from './images/ss-greattaractive/ss_1_en.png'
+import ss_en_2_greattaractive from './images/ss-greattaractive/ss_2_en.png'
+import ss_en_3_greattaractive from './images/ss-greattaractive/ss_3_en.png'
+import ss_en_4_greattaractive from './images/ss-greattaractive/ss_4_en.png'
+import ss_en_5_greattaractive from './images/ss-greattaractive/ss_5_en.png'
+
+import ss_en_1_iosClass from './images/ss-ios-class-for-beginners/ss_1_en.png'
+import ss_en_2_iosClass from './images/ss-ios-class-for-beginners/ss_2_en.png'
+import ss_en_3_iosClass from './images/ss-ios-class-for-beginners/ss_3_en.png'
+import ss_jp_1_iosClass from './images/ss-ios-class-for-beginners/ss_1_jp.png'
+import ss_jp_2_iosClass from './images/ss-ios-class-for-beginners/ss_2_jp.png'
+import ss_jp_3_iosClass from './images/ss-ios-class-for-beginners/ss_3_jp.png'
 
 import regular from './fonts/SF-Regular.otf'
 import italic from './fonts/SF-Italic.otf'
@@ -12,11 +43,89 @@ import boldItalic from './fonts/SF-BoldItalic.otf'
 // MARK: - Images
 
 export const images = {
-  flashcard,
-  stickyMemo,
-  greattaractive,
-  iosClassForBeginners,
-  comingSoon,
+  flashcard: {
+    icon: flashcardIcon,
+    ss: {
+      en: [
+        ss_en_1_flashCard,
+        ss_en_2_flashCard,
+        ss_en_3_flashCard,
+        ss_en_4_flashCard,
+        ss_en_5_flashCard,
+      ],
+      jp: [
+        ss_jp_1_flashCard,
+        ss_jp_2_flashCard,
+        ss_jp_3_flashCard,
+        ss_jp_4_flashCard,
+        ss_jp_5_flashCard,
+      ],
+    }
+  },
+  stickyMemo: {
+    icon: stickyMemoIcon,
+    ss: {
+      en: [
+        ss_en_1_stickyMemo,
+        ss_en_2_stickyMemo,
+        ss_en_3_stickyMemo,
+      ],
+      jp: [
+        ss_jp_1_stickyMemo,
+        ss_jp_2_stickyMemo,
+        ss_jp_3_stickyMemo,
+      ],
+    }
+  },
+  greattaractive: {
+    icon: greattaractiveIcon,
+    ss: {
+      en: [
+        ss_en_1_greattaractive,
+        ss_en_2_greattaractive,
+        ss_en_3_greattaractive,
+        ss_en_4_greattaractive,
+        ss_en_5_greattaractive,
+      ],
+      jp: [
+        ss_en_1_greattaractive,
+        ss_en_2_greattaractive,
+        ss_en_3_greattaractive,
+        ss_en_4_greattaractive,
+        ss_en_5_greattaractive,
+      ],
+    }
+  },
+  iosClassForBeginners: {
+    icon: iosClassForBeginnersIcon,
+    ss: {
+      en: [
+        ss_en_1_iosClass,
+        ss_en_2_iosClass,
+        ss_en_3_iosClass,
+      ],
+      jp: [
+        ss_jp_1_iosClass,
+        ss_jp_2_iosClass,
+        ss_jp_3_iosClass,
+      ],
+    }
+  },
+  comingSoon: {
+    icon: comingSoonIcon,
+    ss: {
+      en: [
+        ss_en_1_iosClass,
+        ss_en_2_iosClass,
+        ss_en_3_iosClass,
+      ],
+      jp: [
+        ss_jp_1_iosClass,
+        ss_jp_2_iosClass,
+        ss_jp_3_iosClass,
+      ],
+    }
+  },
 }
 
 // MARK: - Fonts

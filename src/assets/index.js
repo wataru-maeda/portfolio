a97dbe4232fc5a87243313e3cd0d768f8b94bcdc
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2019-01-28 23:59:44 -0800

    chore: add about section

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,9 @@ import greattaractiveIcon from './images/icon-greattaractive.png'
 import iosClassForBeginnersIcon from './images/ios-class-for-beginners.png'
 import comingSoonIcon from './images/coming-soon.svg'
 import profile from './images/profile.jpg'
+import kiss from './images/kiss.svg'
+import fast from './images/fast.svg'
+import design from './images/design.svg'
 
 import ss_en_1_flashCard from './images/ss-flash-cards/ss_1_en.png'
 import ss_en_2_flashCard from './images/ss-flash-cards/ss_2_en.png'
@@ -132,6 +135,9 @@ export const images = {
     }
   },
   profile,
+  kiss,
+  fast,
+  design,
 }
 
 // MARK: - Fonts
--- src/screens/portfolio/About.js
+++ src/screens/portfolio/About.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import { images } from '../../assets' 
 
 export default class About extends Component {
+  renderItem = (src, desc) => (
+    <div style={styles.itemContainer}>
+      <img src={src} style={styles.itemImage} />
+      <p style={styles.desc}>{desc}</p>
+    </div>
+  )
+
   render() {
     return (
       <div style={styles.container}>
-        <h1 style={styles.title}>About</h1>    
+        <h1 style={styles.title}>About</h1>
+        <div style={styles.contentsContainer}>
+          {this.renderItem(images.kiss, 'KISS (Keep it short and simple), My favorite quote. I always keep it mind when I code.')}
+          {this.renderItem(images.fast, 'Love everything fast. Fast performance, fast coding, fast lerner and the apps I made is fast!')}
+          {this.renderItem(images.design, 'Always keep in mind for the great UI/UX. Following design principle.')}
+        </div>
       </div>
     )
   }
@@ -19,9 +32,32 @@ const styles = {
     background: '#f2f2f2',
     padding: 60,
   },
+  contentsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  itemContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '0 20px',
+  },
+  itemImage: {
+    width: 60,
+    height: 60,
+    margin: '0 0 16px',
+  },
   title: {
     fontSize: '2em',
     color: '#63636e',
     margin: '0 0 30px',
   },
+  desc: {
+    fontSize: '1em',
+    color: '#63636e',
+  }
 }

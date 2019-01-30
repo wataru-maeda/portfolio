647353f5fb291536029f5547b18f88b269cb98e8
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2019-01-29 22:25:28 -0800

    chore: add skills fields

--- src/screens/portfolio/Me.js
+++ src/screens/portfolio/Me.js
@@ -1,41 +1,55 @@
 import React, { Component } from 'react'
 import { images } from '../../assets'
-import Button from '../../components/Button'
+
+const skills = [
+  { name: 'Swift', frequency: 90 },
+  { name: 'Objective-C', frequency: 70 },
+  { name: 'React Native', frequency: 70 },
+  { name: 'React JS', frequency: 60 },
+  { name: 'Node.js', frequency: 30 },
+  { name: 'Java', frequency: 50 },
+  { name: 'UI Design', frequency: 40 },
+]
 
 export default class Me extends Component {
-  render() {
+  renderProfile = () => {
     return (
-      <div style={styles.container}>
-        {this.renderProfile()}
+      <div style={styles.profileContainer}>
+        <img src={images.profile} style={styles.profileImg} />
+        <p style={styles.description}>
+          Hello, my name is Wataru who is creating software for customers.
+          I have created app, web and sometimes APIs for customers since 2014.
+          If you looking for a frontend developer, here I come. Feel free to contact me!
+        </p>
       </div>
     )
   }
 
-  renderProfile = () => {
+  renderSkillItem = ({ name, frequency }) => (
+    <div style={styles.skillItemContainer}>
+      <div style={styles.skillName}>{name}</div>
+      <div style={styles.skillFrequencyContainer}>
+        <div style={{ ...styles.skillFrequency, width: `${frequency}%` }}>
+          {`${frequency} %`}
+        </div>
+      </div>
+    </div>
+  )
+
+  renderSkills = () => (
+    <div style={styles.skillsContainer}>
+      {skills.map(s => this.renderSkillItem(s))}
+    </div>
+  )
+
+  render() {
     return (
-      <div style={styles.profileContainer}>
-        <h1 style={styles.title}>Profile</h1>
-        <img src={images.profile} style={styles.profileImg} />
-        <p style={styles.description}>
-          Hello, my name is Wataru who love creating app/web for customers.
-          I have created software (iOS/Android/Hybrid app/web/APIs..) for customers since 2014.
-          If you looking for a frontend developer who can build modern UI/UX with high performance, here I come. Feel free to contact me!         
-        </p>
-        <Button
-          title="Details of app production request"
-          style={styles.button}
-          onClick={() => { alert('test') }}
-        />
-        <Button
-          title="Details of web production request"
-          style={styles.button}
-          onClick={() => { alert('test') }}
-        />
-        <Button
-          title="Inquiry"
-          style={styles.button}
-          onClick={() => { alert('test') }}
-        />
+      <div style={styles.container}>
+        <h1 style={styles.title}>Profile & Skills</h1>
+        <div style={styles.contentsContainer}>
+          {this.renderProfile()}
+          {this.renderSkills()}
+        </div>
       </div>
     )
   }
@@ -43,21 +57,47 @@ export default class Me extends Component {
 
 const styles = {
   container: {
-    width: '100%',
-    height: '100%',
-    background: '#363e49',
-  },
-  profileContainer: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    width: '100%',
-    height: '100vh',
     padding: 60,
   },
+  contentsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  profileContainer: {
+    width: '40%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  skillsContainer: {
+    width: '50%',
+  },
+  skillItemContainer: {
+    flex: 1,
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '10px 20px',
+  },
+  skillFrequencyContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: '#f8f9fa',
+  },
   description: {
-    color: 'gray',
+    color: '#676772',
     textAlign: 'center',
     fontSize: 18,
     padding: '0 0 40px',
@@ -70,15 +110,22 @@ const styles = {
   profileImg: {
     verticalAlign: 'middle',
     objectFit: 'cover',
-    borderRadius: 50,
-    width: 100,
-    height: 100,
+    borderRadius: 60,
+    width: 120,
+    height: 120,
     margin: '0 0 20px',
   },
-  button: {
-    color: '#3295e3',
-    fontSize: 16,
-    background: 'transparent',
-    height: 44,
+  skillName: {
+    width: 110,
+    fontSize: '0.9em',
+    color: '#676772',
+    textAlign: 'left'
+  },
+  skillFrequency: {
+    background: 'black',
+    textAlign: 'center',
+    fontSize: '0.75em',
+    color: 'white',
+    borderRadius: '0 3px 3px 0'
   },
 }

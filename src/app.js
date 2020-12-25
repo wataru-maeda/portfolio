9615b42bcd027b68c0ae1c4e39f8284cb6c6d47b
WataruMaeda <w.maeda.jp@gmail.com>
2020-12-24 21:34:50 -0800

    chore: update config

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import emailjs from 'emailjs-com'
+import ReactGA from 'react-ga'
 import { loadFontIcons } from 'components/FontIcon'
-import store from './utils/store'
+import store from 'utils/store'
+import { emailJsUserId, gaTrackingID } from 'utils/config'
 import Router from './routes'
 
-emailjs.init('user_6OLHBHCxFZkG5gAMQCE11')
+emailjs.init(emailJsUserId)
 
 class App extends Component {
   componentDidMount() {
     loadFontIcons()
+    ReactGA.initialize(gaTrackingID)
+    ReactGA.pageview(window.location.pathname + window.location.search)
   }
 
   render() {
